b0b631aa9691978301767ee87859bb87b2593441
omorowange isaac <suntimeuganda2019@gmail.com>
2025-07-20 13:26:28 +0300

    9-add_update

--- 9-add.js
+++ 9-add.js
@@ -2,11 +2,5 @@ function add(a, b) {
   return a + b;
 }
 
-const num1 = parseInt(process.argv[2], 10);
-const num2 = parseInt(process.argv[3], 10);
-
-if (isNaN(num1) || isNaN(num2)) {
-  console.log('Arguments must be integers');
-} else {
-  console.log(add(num1, num2));
-}
+const [a, b] = process.argv.slice(2).map(Number);
+console.log(isNaN(a) || isNaN(b) ? 'Arguments must be integers' : add(a, b));
